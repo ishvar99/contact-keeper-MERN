87e1c8d28d57a948b69439fad131eca8c52f6e1c
ishvar99 <ishanvarshney99@gmail.com>
2020-06-16 13:31:37 +0530

    designed contact item component

--- client/src/Components/Layouts/Contacts/ContactItem.jsx
+++ client/src/Components/Layouts/Contacts/ContactItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './ContactItem.css';
 export const ContactItem = ({ contact }) => {
   return (
-    <div>
-      <h3>{contact.name}</h3>
+    <div className='contact-card'>
+      <p>{contact.name}</p>
+      <p>{contact.email}</p>
+      <p>{contact.phone}</p>
+      <div>
+        <button className='btn black'>Edit</button>
+        <button className='btn red'>Delete</button>
+      </div>
     </div>
   );
 };
